d5469c953628f2429373dc7f4377f09c2d9d9b96
Luis Alvarez Sanchez <superluis-14@hotmail.com>
2018-09-24 21:28:17 -0700

    Button Files

---
+++ ClearButton.java
@@ -0,0 +1,26 @@
+
+
+import javax.swing.JButton;
+
+public class ClearButton extends JButton  {
+
+	
+	/**
+	 * Default
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	JButton clearBttn;
+
+	
+	public ClearButton() {
+		clearBttn = new JButton();
+		clearBttn.setText("Clear All");
+	}
+
+	
+	
+	
+	
+}
